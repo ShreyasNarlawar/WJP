060571f23c6dcac9a41eaacca787aa8bdc2f282a
Shreyas <narlawarshreyas@gmail.com>
2024-11-16 21:28:40 +0530

    1611_01

--- Day02_1511_JDBC_Dao/src/com/demo/dao/DBConnection.java
+++ Day02_1511_JDBC_Dao/src/com/demo/dao/DBConnection.java
@@ -6,14 +6,19 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-	public DBConnection() {
-		try {
+	private static Connection conn=null;
+	public static void main(String[] args) throws SQLException {
+	      try {
 			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-			String url = "jdbc:mysql://192.168.10.127:3306/dac86? useSSL=false";
-			Connection cnn = DriverManager.getConnection(url,"dac86","welcome");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			String url="jdbc:mysql://192.168.10.127:3306/dac86?useSSL=false";
+			conn=DriverManager.getConnection(url,"dac86","welcome");
+			if(conn!=null) {
+				System.out.println("Connected");
+			}else
+				System.out.println("Error");
+		   }catch (Exception e) {
+			// TODO: handle exception
+			   e.printStackTrace();
 		}
 	}
 }
